4e3a5838c5c452e2ca24e4846b77f4381b9aa901
Alex Parra <a.parra@proprli.com>
2019-08-04 19:35:07 +0100

    Init Firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
+
+import firebase from 'firebase';
 
 import Header from './components/Header';
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-          <Header />
-          <View style={styles.body}>
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAb5rciElq7_MenS-N8qvDgYJTyXYqHY24',
+      authDomain: 'reactnativeauth-f30b6.firebaseapp.com',
+      databaseURL: 'https://reactnativeauth-f30b6.firebaseio.com',
+      projectId: 'reactnativeauth-f30b6',
+      storageBucket: '',
+      messagingSenderId: '591152581515',
+      appId: '1:591152581515:web:000e08681688f34c',
+    });
+  }
 
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+  render() {
+    return (
+      <Fragment>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+            <Header />
+            <View style={styles.body} />
+          </ScrollView>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
